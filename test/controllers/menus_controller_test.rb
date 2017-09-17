2ded9d52767abe67350cd42d0c79aca3ccb275c6
takayasu-k <takayasu7468@gmail.com>
2017-09-17 17:26:02 +0900

    170917 木曽 不要なテストコードを削除

--- test/controllers/menus_controller_test.rb
+++ test/controllers/menus_controller_test.rb
@@ -6,34 +6,9 @@ class MenusControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should get show" do
-  #   get menu_url
-  #   assert_response :success
-  # end
-
   test "should get new" do
     get new_menu_url
     assert_response :success
   end
-
-  # test "should get create" do
-  #   get menus_create_url
-  #   assert_response :success
-  # end
-
-  # test "should get edit" do
-  #   get edit_menu_url
-  #   assert_response :success
-  # end
-
-  # test "should get update" do
-  #   get menus_update_url
-  #   assert_response :success
-  # end
-
-  # test "should get destroy" do
-  #   get menus_destroy_url
-  #   assert_response :success
-  # end
 
 end
