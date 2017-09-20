810834346ac6d2389b0ef7b150481006bd7e5c7d
takayasu-k <takayasu7468@gmail.com>
2017-09-20 21:02:13 +0900

    20170920 木曽 shop_detailsのnew/create/edit/updateアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get 'shops/:id/menu' => 'shops#shop_menu', as: 'shop_menu'
   resources :menus
   resources :reviews
+  get 'shop_detail/:id/new' => 'shop_details#new', as: 'shop_detail_new'
   resources :shop_details
 end
