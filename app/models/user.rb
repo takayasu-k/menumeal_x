fbea664e2faa1c3b5a5e5daec570d10bd71a654d
Kai Kosuge <grimm_of_fragments@live.jp>
2017-09-25 22:37:55 +0900

    20170925 小菅 ユーザーサインアップ時にuser_profilesデータが生成されるよう変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  has_one :user_profile, dependent: :destroy  
+  after_create :init_user_profile
+
+  def init_user_profile
+    self.create_user_profile!
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
