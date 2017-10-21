9df5cc1dcc11cedce0991365d13cb671ea8f2e12
Kai Kosuge <grimm_of_fragments@live.jp>
2017-10-21 18:25:55 +0900

    20171021 小菅 フォロー関連のアクション等を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
         @user = User.find_by(id: params[:id])
         @user_profile = @user.user_profile
     end
+
+    def following
+        @user = User.find(params[:id])
+        @users = @user.followings
+        render 'show_follow'
+    end
+    
+    def followers
+        @user = User.find(params[:id])
+        @users = @user.followers
+        render 'show_follow'
+    end
 end
