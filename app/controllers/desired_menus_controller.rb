05500f63efe64fc637951886f4e35cfc29131a1a
takayasu-k <takayasu7468@gmail.com>
2017-11-12 20:44:53 +0900

    20171112 木曽 食べたいメニューが登録されない問題を修正

--- app/controllers/desired_menus_controller.rb
+++ app/controllers/desired_menus_controller.rb
@@ -1,7 +1,7 @@
 class DesiredMenusController < ApplicationController
 
   def create
-    @desired_menu = current_user.desired_menus.create(menu_id: params[:id])
+    @desired_menu = current_user.desired_menus.create(menu_id: params[:menu_id])
     redirect_to menu_path(params[:menu_id])
   end
 
