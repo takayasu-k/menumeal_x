64701e2757edac6919ce0a8f0e580216946abe04
Kai Kosuge <grimm_of_fragments@live.jp>
2017-11-12 16:03:23 +0900

    20171112 小菅 アップロード画像のリサイズ機能の追加、ユーザー情報編集画面の変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,6 +14,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  process resize_to_limit: [nil, 300]
+  process resize_to_fill: [300, 300, "Center"]
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
