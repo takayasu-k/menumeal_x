c55677950c16e561efcc4e492f32219d44069d7d
Kai Kosuge <grimm_of_fragments@live.jp>
2017-11-26 15:11:12 +0900

    20171126 小菅 いいねの解除、件数表示を修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,7 @@
 class LikesController < ApplicationController
-  before_action :set_review
+  # before_action :set_review
   def create
+    @review = Review.find_by(id: params[:review_id])
     @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
     @likes = Like.where(review_id: params[:review_id])
     @review_id = params[:review_id]
@@ -9,14 +10,15 @@ class LikesController < ApplicationController
   end
 
   def destroy
+    @review = Review.find_by(id: params[:id])
     @like = Like.find_by(user_id: current_user.id, review_id: params[:id])
     @like.destroy
-    @likes = Like.where(review_id: params[:review_id])
-    @review_id = params[:review_id]
+    @likes = Like.where(review_id: params[:id])
+    @review_id = params[:id]
   end
 
-  private
-    def set_review
-      @review = Review.find_by(id: params[:review_id])
-    end
+  # private
+  #   def set_review
+  #     @review = Review.find_by(id: params[:review_id])
+  #   end
 end
