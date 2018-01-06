bf936218b200da00204c9cf4117c147b59ce7636
guessko <grimm_of_fragments@live.jp>
2018-01-07 01:53:46 +0900

    20170107 小菅 Google Analyticsの適用、font-awesomeのrequire追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #Google Analytics Tracking ID
+  GA.tracker = "UA-112009601-1"
 end
