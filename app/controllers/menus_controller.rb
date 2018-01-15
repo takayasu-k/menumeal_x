7d4cad4bfc36b501b293674565ecd20cb678e5b1
takayasu-k <takayasu7468@gmail.com>
2018-01-15 15:45:42 +0900

    20180115 木曽 メニューの編集機能を追加

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -26,15 +26,17 @@ class MenusController < ApplicationController
   def edit
     @menu = Menu.find_by(id: params[:id])
     @shops = Shop.all
+
+  end
+
+  def update
+    @menu = Menu.find_by(id: params[:id])
     @menu.shop_id = params[:menu][:shop_id]
     @menu.name = params[:menu][:name]
     @menu.price = params[:menu][:price]
     @menu.menu_type = params[:menu][:menu_type]
     @menu.save
     redirect_to shop_menu_path(@menu.shop_id)
-  end
-
-  def update
   end
 
   def destroy
