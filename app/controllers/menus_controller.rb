af83b0aadb895105fa7143547735cf5a0b00372b
takayasu-k <takayasu7468@gmail.com>
2018-01-15 15:15:40 +0900

    20180115 木曽 フッターにログアウトリンクを追加

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -26,6 +26,12 @@ class MenusController < ApplicationController
   def edit
     @menu = Menu.find_by(id: params[:id])
     @shops = Shop.all
+    @menu.shop_id = params[:menu][:shop_id]
+    @menu.name = params[:menu][:name]
+    @menu.price = params[:menu][:price]
+    @menu.menu_type = params[:menu][:menu_type]
+    @menu.save
+    redirect_to shop_menu_path(@menu.shop_id)
   end
 
   def update
