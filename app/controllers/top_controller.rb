4d2eff59121e6ca128f866c57ecf982070c8892f
takayasu-k <takayasu7468@gmail.com>
2018-01-21 19:06:10 +0900

    20180121 木曽 トップページ注目のメニューをレビューから表示するよう修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,7 @@ class TopController < ApplicationController
   def index
     @reviews = Review.order(created_at: :desc).limit(15)
     @menus = Menu.limit(2)
+    @recommended_reviews = Review.order(updated_at: :desc).limit(2)
   end
 
   def about
