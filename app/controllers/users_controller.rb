17077a959fc1943cbac42d4c02877cc13c71eb61
takayasu-k <takayasu7468@gmail.com>
2018-02-11 18:03:04 +0900

    20180211 木曽 SP:ユーザートップ画面で投稿の写真を表示するよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find_by(id: params[:id])
         @user_profile = @user.user_profile
+        @reviews = @user.reviews.order(created_at: :desc)
     end
 
     def following
