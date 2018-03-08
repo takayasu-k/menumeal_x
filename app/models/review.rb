a9b6e04c2a3875439cdd34f7a23311537fd56271
takayasu-k <takayasu7468@gmail.com>
2018-03-08 23:11:15 +0900

    20180308 木曽 reviewモデルにvalidationを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,8 @@ class Review < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   mount_uploader :picture, ImageUploader
+  validates :user_id, presence: true;
+  validates :menu_id, presence: true;
 
   def like_user(user_id)
     likes.find_by(user_id: user_id)
