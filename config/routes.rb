8bb30ad8555a07dde0f5267803478bdb34a9e79f
guessko <grimm_of_fragments@live.jp>
2018-03-18 20:43:24 +0900

    20180318 小菅 店舗会員のモデル、コントローラー、ビューの雛型を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :admin_users, controllers: {
+    sessions:      'admin_users/sessions',
+    passwords:     'admin_users/passwords',
+    registrations: 'admin_users/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
 
   root 'top#index'
   get 'top/index'
